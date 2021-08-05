835964e52560f00c758b9554b1a3edc2d507a154
uzowihek <uzowihek@gmail.com>
2021-08-05 08:27:13 -0700

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import {KitchenComponent} from './kitchen/kitchen.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, KitchenComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
